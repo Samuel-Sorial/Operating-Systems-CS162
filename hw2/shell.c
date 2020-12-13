62a6f0ff404811bf39eba56ecdda058ede6157ef
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-12-13 22:22:13 +0200

    Implementing print current working directory

--- hw2/shell.c
+++ hw2/shell.c
@@ -30,7 +30,7 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
-
+int cmd_pwd(struct tokens *tokens);
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
 
@@ -44,7 +44,16 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "print the current working directory to the STD"}
 };
+
+/* Prints the current workgin directory */
+int cmd_pwd(unused struct tokens *tokens){
+  char buffer[90];
+  getcwd(buffer, sizeof(buffer));
+  fprintf(stdout, "%s\n", buffer);
+  return 1;
+}
 
 /* Prints a helpful description for the given command */
 int cmd_help(unused struct tokens *tokens) {
